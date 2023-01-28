1f706c21fca84e8703a5841b37bc961cf8bf5e7e
BagaBaga <bdesign@bk.ru>
2023-01-28 03:07:45 +0300

    weoefrn

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat catBean = applicationContext.getBean("cat", Cat.class);
+
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean == bean2);
+        System.out.println(catBean == catBean2);
     }
 }
